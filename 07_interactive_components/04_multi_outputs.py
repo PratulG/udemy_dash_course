af7dabddb7c91cddb77c62956a8fe2f5cb3522dd
PratulG <39401239+PratulG@users.noreply.github.com>
2022-09-14 19:36:17 +0530

    Update 04_multi_outputs.py

--- 07_interactive_components/04_multi_outputs.py
+++ 07_interactive_components/04_multi_outputs.py
@@ -1,17 +1,23 @@
-import dash
-import dash_html_components as html
-import dash_core_components as dcc
-import plotly.graph_objs as go
-from dash.dependencies import Input, Output
+import plotly.graph_objects as go # or plotly.express as px
+fig = go.Figure() # or any Plotly Express function e.g. px.bar(...)
+# fig.add_trace( ... )
+# fig.update_layout( ... )
+
 import pandas as pd
+import numpy as np
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
 import base64
 
+
 # Load the data
-df = pd.read_csv("wheels.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/PratulG/udemy_dash_course/master/07_interactive_components/wheels.csv")
 
 def encode_image(image_file):
-  encoded = base64.b64encode(open(image_file, "rb").read())
-  return "data:image/png;base64,{}".format(encoded.decode())
+    encoded = base64.b64encode(open(image_file, "rb").read())
+    return "data:image/png;base64,{}".format(encoded.decode())
 
 # Instanciate the app
 app = dash.Dash()
@@ -55,14 +61,14 @@ app.layout = html.Div(
   Input(component_id = "wheels", component_property = "value")
 )
 def callback_a(wheels):
-  return f"You chose {wheels}"
+    return f"You chose {wheels}"
 
 @app.callback(
   Output(component_id = "colors-output", component_property = "children"),
   Input(component_id = "colors", component_property = "value")
 )
 def callback_b(colors):
-  return f"You chose {colors}"
+    return f"You chose {colors}"
 
 @app.callback(
   Output(component_id = "display-image", component_property = "src"),
@@ -70,12 +76,11 @@ def callback_b(colors):
   Input(component_id = "colors", component_property = "value")
 )
 def callback_image(wheels, color):
-  path = "images/"
-  encoded = encode_image(
+    path = "images/"
+    encoded = encode_image(
     path + df[(df["wheels"] == wheels) & (df["color"] == color)]["image"].values[0]
   )
-  return encoded
+    return encoded
 
-# Run the app
-if __name__ == "__main__":
-  app.run_server(debug = True)
+
+app.run_server(debug=True, use_reloader=False, port = 8050)  # Turn off reloader if inside Jupyter
