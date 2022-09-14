df295e287371816eb47384a66c35765ade41c4a2
PratulG <39401239+PratulG@users.noreply.github.com>
2022-09-14 17:21:34 +0530

    Update 01_hello_dash.py

--- 04_dash_basics/01_hello_dash.py
+++ 04_dash_basics/01_hello_dash.py
@@ -1,15 +1,16 @@
+import plotly.graph_objects as go # or plotly.express as px
+fig = go.Figure() # or any Plotly Express function e.g. px.bar(...)
+# fig.add_trace( ... )
+# fig.update_layout( ... )
+
 import dash
-import dash_html_components as html
 import dash_core_components as dcc
+import dash_html_components as html
 
-# Instanciate the app
 app = dash.Dash()
-
 # Define de layout
 app.layout = html.Div([
   html.H1("Hello Dash!")
 ])
 
-# Run server
-if __name__ == "__main__":
-  app.run_server(debug = True)
+app.run_server(debug=True, use_reloader=False)  # Turn off reloader if inside Jupyter
