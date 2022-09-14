0419e16c574c605bbb791773e22ee0bea2bef588
PratulG <39401239+PratulG@users.noreply.github.com>
2022-09-14 19:46:37 +0530

    Update 01_state.py

--- 08_callbacks_with_state/01_state.py
+++ 08_callbacks_with_state/01_state.py
@@ -1,7 +1,16 @@
+import plotly.graph_objects as go # or plotly.express as px
+fig = go.Figure() # or any Plotly Express function e.g. px.bar(...)
+# fig.add_trace( ... )
+# fig.update_layout( ... )
+
+import pandas as pd
+import numpy as np
 import dash
-import dash_html_components as html
 import dash_core_components as dcc
+import dash_html_components as html
 from dash.dependencies import Input, Output, State
+import base64
+
 
 # Instanciate the app
 app = dash.Dash()
@@ -35,8 +44,6 @@ app.layout = html.Div([
   State(component_id = "number-in", component_property = "value")
 )
 def multiply(n_clicks, number):
-  return f"{number} was typed in and btn was clicked {n_clicks} times"
+    return f"{number} was typed in and btn was clicked {n_clicks} times"
 
-# Run the app
-if __name__ == "__main__":
-  app.run_server(debug = True)
+app.run_server(debug=True, use_reloader=False)  # Turn off reloader if inside Jupyter
